220315d4728d05d82d647ae10a53eeb1c1ed8d37
Diego Oliveira <diegoc.oliveira@gmail.com>
2022-06-08 06:52:32 -0300

    add-javascript-aula4-questao4

---
+++ front-end/javascript/aula03/javascript/script.js
@@ -0,0 +1,23 @@
+const result = document.querySelector('#result');
+
+function calcular(){
+    const num1 = document.querySelector('#num1').value;
+    const num2 = document.querySelector('#num2').value;
+    const operador = document.querySelector('#operators').value;
+    const total = document.querySelector('#total');
+    
+    if(operador == '+'){
+        total.value = parseFloat(num1) + parseFloat(num2);
+    }else if(operador == '-'){
+        total.value = parseFloat(num1) - parseFloat(num2);
+    }else if(operador == '*'){
+        total.value = parseFloat(num1) * parseFloat(num2);
+    }else if(operador == '/'){
+        total.value = parseFloat(num1) / parseFloat(num2);
+    }
+    
+
+   
+}
+
+result.onclick = calcular;
